4ba5371e03d4ed7d6fc9fcae11bcd2ab1f15423b
León van de Pavert <leon.van.de.pavert@intrum.com>
2018-11-22 17:32:34 +0200

    54:58 Conditional Rendering, initial clean-up

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -8,26 +8,14 @@ class Counter extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+      <div>
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
-      </React.Fragment>
+      </div>
     );
-  }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
