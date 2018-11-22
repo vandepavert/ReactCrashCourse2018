62c57e9242e4044d27eca84aa3d5bf3b19d836aa
León van de Pavert <leon.van.de.pavert@intrum.com>
2018-11-22 14:42:27 +0200

    40:49 Setting Attributes, removed inline styles

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,17 +5,11 @@ class Counter extends Component {
     count: 0
   };
 
-  styles = { fontSize: 50, fontWeight: "bold" };
-
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button style={{ fontSize: 30 }} className="btn btn-secondary btn-sm">
-          Increment
-        </button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
