39003f5b35431b04cd608c0b0944946060c99e13
leon.van.de.pavert@lindorff.com <leon.van.de.pavert@intrum.com>
2019-01-15 18:01:46 +0200

    Revert "1:21:18 Passing Data to Components"
    This reverts commit 778136f5f17c1b4df956c18cf3d1bc0363f07096.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,21 +5,16 @@ class Counter extends Component {
     count: 0
   };
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
+  handleIncrement() {
+    console.log("Increment Clicked", this);
   }
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
